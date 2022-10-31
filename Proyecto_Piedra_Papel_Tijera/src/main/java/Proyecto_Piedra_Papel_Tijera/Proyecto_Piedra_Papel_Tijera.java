223ded60a8d52e3069d877faf7cf41f717db1d81
Archimarks <marcoscollazos3@gmail.com>
2022-10-30 19:31:19 -0600

    Primer commit

---
+++ Proyecto_Piedra_Papel_Tijera/src/main/java/Proyecto_Piedra_Papel_Tijera/Proyecto_Piedra_Papel_Tijera.java
@@ -0,0 +1,12 @@
+package Proyecto_Piedra_Papel_Tijera;
+
+
+/**
+ * @author Marcos Alejandro Collazos Marmolejo
+ */
+public class Proyecto_Piedra_Papel_Tijera {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
